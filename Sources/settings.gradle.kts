e926e76271880d2e426cea1ff1aa64551b8ee8ed
Arnaud Roland <arnaud.roland@batch.com>
2025-06-05 11:49:29 +0200

    Update settings.gradle.kts

--- Sources/settings.gradle.kts
+++ Sources/settings.gradle.kts
@@ -4,6 +4,13 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        kotlin("jvm") version "1.9.0"
+        id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
@@ -12,5 +19,5 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-rootProject.name = "Batch"
-include (":sdk", ":sdk-stubs", ":sdk-processor", ":sdk-lint")
+rootProject.name = "Batch Android SDK"
+include (":sdk", ":sdk-sample", ":sdk-stubs", ":sdk-processor", ":sdk-lint", ":dokka-limit-public-api")
